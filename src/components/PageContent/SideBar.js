3d5077e4d3a5e638b8ea6188de43ab3c597e3d39
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-02-12 18:44:07 -0300

    feat: :sparkles: LogIn
    issue #1

--- src/components/PageContent/SideBar.js
+++ src/components/PageContent/SideBar.js
@@ -1,91 +1,122 @@
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../providers/auth";
-
+import { useContext } from "react";
 import { IconContext } from "react-icons";
-import { FaRegWindowClose } from "react-icons/fa";
+import { FaRegWindowClose, FaSignOutAlt } from "react-icons/fa";
+import LogIn from "./LogIn.js";
+import SignUp from "./SignUp.js";
 
 export default function SideBar() {
+  const { userData } = useContext(AuthContext);
+  const { showLogIn } = useContext(AuthContext);
+  const { showSignUp } = useContext(AuthContext);
+  const { setSideBarTrigger } = useContext(AuthContext);
 
-    const { sideBarTrigger, setSideBarTrigger } = React.useContext(AuthContext);
+  const navigate = useNavigate();
 
+  function logOut() {
+    localStorage.clear();
+    navigate(0);
+  }
 
-    return (
-        <Container onClick={(sideBarTrigger) => setSideBarTrigger(false)}>
-            <IconContext.Provider value={{ color: "#FFFFFF", size: "46px" }}>
-                <FaRegWindowClose onClick={() => setSideBarTrigger(false)} />
-            </IconContext.Provider>
-            <Content>
-                <SideHeader>Olá, faça seu login!</SideHeader>
-                <SideButton><h1>Todas as categorias</h1></SideButton>
-                <SideButton>Futebol</SideButton>
-                <SideButton>Automobilismo</SideButton>
-                <SideButton>Handebol</SideButton>
-                <SideButton>Basquete</SideButton>
-                <SideButton>Ciclismo</SideButton>
-            </Content>
-        </Container>
-    );
+  return (
+    <Container>
+      <IconContext.Provider value={{ color: "#FFFFFF", size: "46px" }}>
+        <FaRegWindowClose onClick={() => setSideBarTrigger(false)} />
+      </IconContext.Provider>
+      {userData ? (
+        <IconContext.Provider value={{ color: "#FFFFFF", size: "46px" }}>
+          <IconBox>
+            <FaSignOutAlt onClick={() => logOut()} />
+          </IconBox>
+        </IconContext.Provider>
+      ) : null}
+      <Content>
+        <SideHeader>
+          Olá, {userData ? userData.name : "faça seu login!"}
+        </SideHeader>
+        {showLogIn ? <LogIn /> : null}
+        {showSignUp ? <SignUp /> : null}
+        <SideButton>
+          <h1>Todas as categorias</h1>
+        </SideButton>
+        <SideButton>Futebol</SideButton>
+        <SideButton>Automobilismo</SideButton>
+        <SideButton>Handebol</SideButton>
+        <SideButton>Basquete</SideButton>
+        <SideButton>Ciclismo</SideButton>
+      </Content>
+    </Container>
+  );
 }
 // ::::::::::Styled-Components::::::::::
 const Container = styled.div`
-    width: 100%;
-    min-height: 100vh;
+  width: 100%;
+  min-height: 100vh;
 
-    display: flex;
-    position: fixed;
-    z-index: 3;
-`
+  display: flex;
+  position: fixed;
+  z-index: 3;
+`;
 const Content = styled.div`
-    width: 300px;
-    min-height: 100vh;
-    background-color: #1e1e1e;
+  width: 300px;
+  min-height: 100vh;
+  background-color: #1e1e1e;
 
-    position: fixed;
-    right: 0;
+  position: fixed;
+  right: 0;
 
-    &:first-child{
-        color: red;
-    }
-`
+  &:first-child {
+    color: red;
+  }
+`;
 const SideHeader = styled.div`
-    width: 100%;
-    height: 70px;
-    padding: 20px;
-    background-color: #1b1b1b;
-    
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    
-    font-family: 'Montserrat', sans-serif;
-    font-size: 18px;
-    color: #FFF;
-`
-const SideButton = styled.div`
-    width: 100%;
-    height: 70px;
-    background: #262626;
-    border: 1px solid #262626;
-    border-radius: 5px;
-    border-bottom: 1px solid black;
-    border-top: 1px solid rgba(255,255,255,.1);
-    border-left: 5px solid transparent;
-    padding: 25px;
+  width: 100%;
+  height: 70px;
+  padding: 20px;
+  background-color: #1b1b1b;
 
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
- 
-    font-family: 'Montserrat', sans-serif;
-    font-size: 20px;
-    color: #FFF;
-    
-    &:first-child{
-        color: red;
-    }
-    
-    h1{
-        font-size: 25px;
-    }
-`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  font-family: "Montserrat", sans-serif;
+  font-size: 18px;
+  color: #fff;
+`;
+const SideButton = styled.div`
+  width: 100%;
+  height: 70px;
+  background: #262626;
+  border: 1px solid #262626;
+  border-radius: 5px;
+  border-bottom: 1px solid black;
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+  border-left: 5px solid transparent;
+  padding: 25px;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  font-family: "Montserrat", sans-serif;
+  font-size: 20px;
+  color: #fff;
+
+  &:first-child {
+    color: red;
+  }
+
+  h1 {
+    font-size: 25px;
+  }
+`;
+const IconBox = styled.div`
+  position: absolute;
+  cursor: pointer;
+  right: 10px;
+  z-index: 2;
+  top: 12px;
+`;
